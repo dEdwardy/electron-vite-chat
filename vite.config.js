58e9e302bede40fe816923be330bdc19f5add9b3
edw4rd <957196958@qq.com>
2021-05-11 16:23:05 +0800

    antd按需加载且无需注册component

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,30 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { SvgBuilder } from './src/render/plugins/SvgBuilder'
+import ViteComponents from 'vite-plugin-components'
+import styleImport from 'vite-plugin-style-import'
 import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    SvgBuilder('./src/render/icons/svg/') // 这里已经将src/icons/svg/下的svg全部导入，无需再单独导入
+    ViteComponents(),
+    SvgBuilder('./src/render/icons/svg/'), // 这里已经将src/icons/svg/下的svg全部导入，无需再单独导入
+    styleImport({
+      libs: [
+        {
+          libraryName: 'ant-design-vue',
+          esModule: true,
+          resolveStyle: (name) => {
+            return `ant-design-vue/es/${name}/style/index`
+          }
+        }
+      ]
+    })
   ],
-  resolve:{
+  resolve: {
     alias: {
-      '@': resolve(__dirname, 'src/render'),
-    },
+      '@': resolve(__dirname, 'src/render')
+    }
   }
 })
